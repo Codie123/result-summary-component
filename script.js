dcc6848d6892b15a7fbea28e8862a948666de52c
Codie@123 <elliott6518@gmail.com>
2023-06-06 15:33:49 +0530

    Updated by Codie123
    Completed

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
 // api to fecth the data from the data.json
 //generate function which will display the data
-const data = [
-  {
-    category: "Reflex",
-    score: 80,
-    icon: "./assets/images/icon-reaction.svg",
-  },
-  {
-    category: "Memory",
-    score: 92,
-    icon: "./assets/images/icon-memory.svg",
-  },
-  {
-    category: "Verbal",
-    score: 55,
-    icon: "./assets/images/icon-verbal.svg",
-  },
-  {
-    category: "Visual",
-    score: 72,
-    icon: "./assets/images/icon-visual.svg",
-  },
-];
 
-const cate = document.querySelectorAll(".category");
+getJsonData();
+
+async function getJsonData() {
+  const response = await fetch("./data.json");
+  const data = await response.json();
+  console.log(data);
+  processData(data);
+}
+
+function processData(x) {
+  x.forEach((item) => {
+    generate(item);
+  });
+}
+
+// const cate = document.querySelectorAll(".category");
 const summaryData = document.querySelector("#summary-data");
-
-data.forEach((x) => {
-  generate(x);
-});
 
 function generate(data) {
   const divEl = document.createElement("div");
-  console.log(data.icon);
+
   divEl.classList.add("items");
-  divEl.innerHTML = ` 
-    <h5 class="category"><img  class="icon" src="${data.icon}" alt="Lighting bolt"> ${data.category}</h5>
-    <div class="data">
-      <p class="score" >${data.score}</p> /100
-    </div>`;
+  divEl.setAttribute("id", `${data.color}`);
+  divEl.innerHTML = `
+  
+            <div class="category" >
+              <h5 >${data.category}</h5>
+              <img  class="icon" src="${data.icon}" alt="Lighting bolt"> 
+            </div>
+    
+            <div class="data">
+              <p class="score" >${data.score} <span>/100</span></p> 
+            </div>`;
+
   summaryData.appendChild(divEl);
 }
